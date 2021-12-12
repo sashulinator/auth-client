6dd996fd1f8039b072e70301e425e57c0db236ec
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2021-12-12 16:10:18 +0400

    init

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom'
+
+export const App = () => {
+  return (
+    <>
+      <h1>Я влюбился</h1>
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
