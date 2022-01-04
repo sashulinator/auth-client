1163a7f3832f1da6dedbad38674993fed742d52a
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2022-01-04 14:29:10 +0400

    add table for users

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { Link } from 'react-router-dom'
 import cx from 'clsx'
 import ROUTES from '../../constants/routes'
-
+import { DetailsList, SelectionMode } from '@fluentui/react/lib/DetailsList'
 type UsersProps = {
   className?: string
 }
@@ -14,6 +14,21 @@ const Users: FC<UsersProps> = ({ className }): JSX.Element => {
       <nav>
         <Link to={ROUTES.LOGIN.buildURL()}>Login</Link>
       </nav>
+      <DetailsList
+        items={[{ first: 'one', second: 'two' }]}
+        columns={[
+          {
+            key: 'keyfirst',
+            name: 'firstname',
+            minWidth: 100,
+            fieldName: 'first',
+          },
+        ]}
+        selectionMode={SelectionMode.none}
+        ariaLabelForSelectionColumn="Toggle selection"
+        ariaLabelForSelectAllCheckbox="Toggle selection for all items"
+        checkButtonAriaLabel="select row"
+      />
     </div>
   )
 }
