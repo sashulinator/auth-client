b5e26065968d5a4f9f1eceda83b37eee8d4f1430
savchenko alexander <ext.adsavchenko@tinkoff.ru>
2022-01-05 19:31:41 +0400

    add history

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,16 @@
 import ReactDOM from 'react-dom'
-import { BrowserRouter } from 'react-router-dom'
+import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import RootLayer from './layer'
 import store from './redux-store'
+import history from './history'
 
 export const App = () => {
   return (
     <Provider store={store}>
-      <BrowserRouter>
+      <HistoryRouter history={history}>
         <RootLayer />
-      </BrowserRouter>
+      </HistoryRouter>
     </Provider>
   )
 }
