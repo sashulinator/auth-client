aae0fd0fe80c7475d44c4b749348cafc2a149380
Savchenko Alexander <sashulinator@gmail.com>
2022-04-22 19:03:56 +0400

    add property panel

--- src/pages/form-constructor/index.tsx
+++ src/pages/form-constructor/index.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 import Preview from './preview'
+import PropertyPanel from './property-panel'
 import TreePanel from './tree-panel'
 import React, { FC } from 'react'
 
@@ -8,6 +9,7 @@ const FormConstructor: FC = (): JSX.Element => {
     <div className="FormConstructor">
       <TreePanel />
       <Preview />
+      <PropertyPanel />
     </div>
   )
 }
---
+++ src/pages/form-constructor/property-panel.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const PropertyPanel: FC = (): JSX.Element => {
+  return <div className="PropertyPanel">PropertyPanel</div>
+}
+
+export default PropertyPanel
