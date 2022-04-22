74cbb22d51531d0768cf56835a65ea68d7a4042c
Savchenko Alexander <sashulinator@gmail.com>
2022-04-22 20:52:06 +0400

    draw preview forms from schema

--- src/pages/form-constructor/preview.tsx
+++ src/pages/form-constructor/preview.tsx
@@ -1,7 +1,29 @@
+import { PrimaryButton, Stack } from '@fluentui/react'
+import { isString } from '@savchenko91/schema-validator'
+
+import formSchema from './form-schema.json'
 import React, { FC } from 'react'
 
+const hashComponents = {
+  Stack,
+  PrimaryButton,
+} as any
+
 const Preview: FC = (): JSX.Element => {
-  return <div className="Preview">Preview</div>
+  return <div className="Preview">{formSchema.children.map(drawChildren)}</div>
+}
+
+const drawChildren = (item: any) => {
+  if (isString(item)) {
+    return item
+  }
+  const Comp = hashComponents[item.name]
+  let drawedChildren
+  if (item.children) {
+    drawedChildren = item.children.map(drawChildren)
+  }
+
+  return <Comp {...item.props}>{drawedChildren}</Comp>
 }
 
 export default Preview
