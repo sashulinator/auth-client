eadc928f763c5c17d88e89bb435a2480bfb7d422
Savchenko Alexander <sashulinator@gmail.com>
2022-04-26 10:40:09 +0400

    form schema id changed

--- src/recoil/form-schema.mock.ts
+++ src/recoil/form-schema.mock.ts
@@ -20,16 +20,11 @@ export const formSchemaMock = {
           padding: '45px 40px',
         },
       },
-      children: [
-        'ee4254ef-9009-4243-be68-51ce733b338e',
-        'ee4254ef-5555-4243-be68-51ce733b338e',
-        'ee4234ef-9099-8943-8968-54ce7subject',
-        'ee4234ef-9099-8943-8968-54ce73object',
-      ],
+      children: ['buttonOneId', 'buttonTwoId', 'textInputOneId', 'textInputTwoId'],
     },
     {
       path: 'hello12',
-      id: 'ee4254ef-9009-4243-be68-51ce733b338e',
+      id: 'buttonOneId',
       name: 'КнопкаГлавная1',
       componentSchemaId: 'ee4254ef-9099-4243-be68-51ce733b3376',
       componentName: 'PrimaryButton',
@@ -41,7 +36,7 @@ export const formSchemaMock = {
       },
     },
     {
-      id: 'ee4254ef-5555-4243-be68-51ce733b338e',
+      id: 'buttonTwoId',
       name: 'КнопкаГлавная2',
       componentSchemaId: 'ee4254ef-9099-4243-be68-51ce733b3376',
       componentName: 'PrimaryButton',
@@ -53,7 +48,7 @@ export const formSchemaMock = {
       },
     },
     {
-      id: 'ee4234ef-9099-8943-8968-54ce7subject',
+      id: 'textInputOneId',
       name: 'ТекстовоеПоле1',
       componentSchemaId: 'ee4234ef-9099-8943-8968-51ce733b870',
       componentName: 'TextField',
@@ -64,12 +59,12 @@ export const formSchemaMock = {
         {
           events: ['onInit'],
           actions: ['setValue'],
-          componentIds: ['ee4234ef-9099-8943-8968-54ce73object'],
+          componentIds: ['textInputTwoId'],
         },
       ],
     },
     {
-      id: 'ee4234ef-9099-8943-8968-54ce73object',
+      id: 'textInputTwoId',
       name: 'ТекстовоеПоле2',
       componentSchemaId: 'ee4234ef-9099-8943-8968-51ce733b870',
       componentName: 'TextField',
