b11ee8790868fab837ba9feee57c4bee92f58271
Savchenko Alexander <sashulinator@gmail.com>
2022-04-29 20:56:39 +0400

    remove redux

--- src/redux/user.constants.ts
+++
@@ -1,3 +0,0 @@
-import { createAPIconstants } from '@/utils/create-api-constants'
-
-export default createAPIconstants('user', 'GET', 'CREATE', 'UPDATE', 'PRUNE_MANY', 'GET_LIST')
--- src/redux/user.selector.ts
+++
@@ -1,5 +0,0 @@
-import { RootState, UserState } from '@/types/redux-state'
-
-export const getList = (s: RootState): UserState['getList'] => s.user.getList
-export const create = (s: RootState): UserState['create'] => s.user.create
-export const update = (s: RootState): UserState['create'] => s.user.update
