4c908814314d33350922d3e4db6477a18bd2933d
Savchenko Alexander <sashulinator@gmail.com>
2022-05-01 23:21:39 +0400

    add create form button

--- src/pages/schema-list/index.tsx
+++ src/pages/schema-list/index.tsx
@@ -1,19 +1,30 @@
-import { Stack } from '@fluentui/react'
+import { PrimaryButton, Stack } from '@fluentui/react'
 
 import List from './list'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
+import ROUTES from '@/constants/routes'
 import Header from '@/widgets/header'
 
-function SchemaListPage(): JSX.Element {
+export default function SchemaListPage(): JSX.Element {
+  const navigate = useNavigate()
+
   return (
     <>
       <Header />
       <Stack as="main" className="SchemaList">
+        <Stack horizontal tokens={{ padding: '20px 20px' }} horizontalAlign="end">
+          <PrimaryButton
+            onClick={() => {
+              navigate(ROUTES.FORM_CONSTRUCTOR.buildURL())
+            }}
+          >
+            Create new
+          </PrimaryButton>
+        </Stack>
         <List />
       </Stack>
     </>
   )
 }
-
-export default SchemaListPage
--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -3,7 +3,7 @@ import { Stack } from '@fluentui/react/lib/Stack'
 import './header.css'
 
 import React from 'react'
-import { useTranslation } from 'react-i18next'
+// import { useTranslation } from 'react-i18next'
 import { Link, useLocation } from 'react-router-dom'
 
 import ROUTES, { getCurrentRoute } from '@/constants/routes'
@@ -12,21 +12,16 @@ import LanguageDropdown from '@/shared/language-dropdown'
 import ThemeDropdown from '@/shared/theme'
 
 export default function Header(): JSX.Element {
-  const { t } = useTranslation()
+  // const { t } = useTranslation()
 
   // ререндерит header при изменении пути
   useLocation()
-
-  const formConstructorText = t('pagesNames.formConstructor')
 
   const isFormConstructorPage = getCurrentRoute()?.PATH === ROUTES.FORM_CONSTRUCTOR.PATH
 
   return (
     <Stack as="header" horizontal horizontalAlign="space-between" className="Header">
       <Stack as="ul" horizontal verticalAlign="center" tokens={{ childrenGap: 10, padding: '15px 40px' }}>
-        <li>
-          <Link to={ROUTES.FORM_CONSTRUCTOR.buildURL()}>{formConstructorText}</Link>
-        </li>
         <li>
           <Link to={ROUTES.SCHEMA_LIST.buildURL()}>Schemas</Link>
         </li>
