77d1af5a2e3fed404e45fb6c35a9040c5ba73ea7
Savchenko Alexander <sashulinator@gmail.com>
2022-05-04 10:04:33 +0400

    add onKeyDeleteListener

--- src/pages/form-constructor/tree-panel/ui/tree-panel.tsx
+++ src/pages/form-constructor/tree-panel/ui/tree-panel.tsx
@@ -1,6 +1,6 @@
 import { TreeDestinationPosition, TreeSourcePosition, moveItemOnTree } from '@atlaskit/tree'
 import { FontIcon, PrimaryButton } from '@fluentui/react'
-import { assertNotUndefined } from '@savchenko91/schema-validator'
+import { assertNotNull, assertNotUndefined } from '@savchenko91/schema-validator'
 
 import './index.css'
 
@@ -9,10 +9,10 @@ import { buildTree } from '../lib/build-tree'
 import { highlightComponent, removeHighlight } from '../lib/highlight'
 import React, { useEffect, useState } from 'react'
 import PerfectScrollbar from 'react-perfect-scrollbar'
-import { useRecoilState } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
-import { moveComps as moveComp } from '@/helpers/form-schema-state'
-import { FSchemaState, pickedFCompIdState } from '@/pages/form-constructor/preview/model/form-schema'
+import { moveComps as moveComp, removeComp } from '@/helpers/form-schema-state'
+import { FSchemaState, pickedFCompIdState, pickedFCompState } from '@/pages/form-constructor/preview/model/form-schema'
 import Tree from '@/shared/tree'
 
 function TreePanel(): JSX.Element {
@@ -20,8 +20,9 @@ function TreePanel(): JSX.Element {
   const [pickedFCompId, setPickedFCompId] = useRecoilState(pickedFCompIdState)
   const [, setPaletteOpen] = useRecoilState(paletteModalState)
   const [tree, setTree] = useState(rebuildTree)
-
+  const pickedFComp = useRecoilValue(pickedFCompState)
   useEffect(() => setTree(rebuildTree), [FSchema, pickedFCompId])
+  useEffect(addDeleteKeyListener, [pickedFCompId])
 
   function rebuildTree() {
     return buildTree(FSchema?.comps, {
@@ -32,6 +33,39 @@ function TreePanel(): JSX.Element {
       onBlur: removeHighlight,
       onMouseLeave: removeHighlight,
     })
+  }
+
+  function addDeleteKeyListener() {
+    function test(event: any): void {
+      if (event.key === 'Backspace') {
+        assertNotNull(pickedFComp)
+        assertNotNull(FSchema)
+
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        if ((document?.activeElement as any)?.type === 'text') {
+          return
+        }
+
+        const comps = removeComp(pickedFComp?.id, FSchema.comps)
+        setPickedFCompId('')
+        setTimeout(() => {
+          setFSchema({ ...FSchema, comps })
+          removeHighlight()
+        })
+      }
+    }
+
+    if (pickedFCompId) {
+      document.removeEventListener('keydown', test)
+      document.addEventListener('keydown', test)
+    } else {
+      document.removeEventListener('keydown', test)
+    }
+
+    // Удаляем слушатель при уничтожении компонента
+    return () => {
+      document.removeEventListener('keydown', test)
+    }
   }
 
   function onDragEnd(from: TreeSourcePosition, to?: TreeDestinationPosition) {
