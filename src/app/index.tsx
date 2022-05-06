7cc1d60babe9086668ba0ae27302c2ad42ba9652
Savchenko Alexander <sashulinator@gmail.com>
2022-05-06 13:01:21 +0400

    root

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,11 @@
 import { initializeIcons } from '@fluentui/react'
 
+import ErrorBoundary from './error-boundary'
 import history from './history'
 import './i18n'
 import RootLayer from './layer'
 import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom'
+import { createRoot } from 'react-dom/client'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import { RecoilRoot } from 'recoil'
@@ -18,18 +19,26 @@ export const App = () => {
 
   return (
     <Suspense>
-      <QueryClientProvider client={queryClient}>
-        <RecoilRoot>
-          <ThemeProvider>
-            <HistoryRouter history={history}>
-              <RootLayer />
-            </HistoryRouter>
-          </ThemeProvider>
-        </RecoilRoot>
-      </QueryClientProvider>
+      <ErrorBoundary>
+        <QueryClientProvider client={queryClient}>
+          <RecoilRoot>
+            <ThemeProvider>
+              <HistoryRouter history={history}>
+                <RootLayer />
+              </HistoryRouter>
+            </ThemeProvider>
+          </RecoilRoot>
+        </QueryClientProvider>
+      </ErrorBoundary>
     </Suspense>
   )
 }
 
+const rootElement = document.getElementById('root')
+
+if (rootElement) {
+  const root = createRoot(rootElement)
+  root.render(<App />)
+}
+
 // eslint-disable-next-line import/no-named-as-default-member
-ReactDOM.render(<App />, document.getElementById('root'))
