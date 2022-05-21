c4b304b08e2b325fb303872f8d6d2ffc9257185e
Savchenko Alexander <sashulinator@gmail.com>
2022-05-21 20:07:29 +0400

    Add icon

---
+++ src/app/register-icons.tsx
@@ -0,0 +1,14 @@
+import { registerIcons } from '@fluentui/react/lib/Styling'
+
+import React from 'react'
+
+registerIcons({
+  icons: {
+    More: (
+      <svg width="1em" height="1em" viewBox="0 0 105 35" fill="currentColor">
+        <circle cx={17.5} cy={17.5} r={17.5} />
+        <circle cx={87.5} cy={17.5} r={17.5} />
+      </svg>
+    ),
+  },
+})
