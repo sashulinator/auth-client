421bc487052e621fcf2d81be48661664c53acb0c
Savchenko Alexander <sashulinator@gmail.com>
2022-06-01 16:23:46 +0400

    add width

--- src/shared/horizontal-line/ui/horizontal-line.tsx
+++ src/shared/horizontal-line/ui/horizontal-line.tsx
@@ -4,14 +4,14 @@ import React from 'react'
 
 interface HorizontalLineProps {
   color: string
-  margin: string
   label: string
+  leftWidth: string
+  rightWidth: string
 }
 
 export default function HorizontalLine(props: HorizontalLineProps): JSX.Element {
   return (
     <Stack horizontal verticalAlign="center">
-      {props.label && <Label styles={{ root: { padding: '0 8px 3px' } }}>{props.label}</Label>}
       <hr
         style={{
           boxSizing: 'border-box',
@@ -20,7 +20,20 @@ export default function HorizontalLine(props: HorizontalLineProps): JSX.Element 
           opacity: '0.3',
           background: 'var(--black)',
           margin: '0',
-          width: '100%',
+          width: props.leftWidth || '24px',
+          color: props.color,
+        }}
+      />
+      {props.label && <Label styles={{ root: { padding: '0 4px', margin: '0' } }}>{props.label}</Label>}
+      <hr
+        style={{
+          boxSizing: 'border-box',
+          border: '0',
+          height: '1px',
+          opacity: '0.3',
+          background: 'var(--black)',
+          margin: '0',
+          width: props.rightWidth || '100%',
           color: props.color,
         }}
       />
