b4bf89347898aabe536cee51508604539f415ebd
Savchenko Alexander <sashulinator@gmail.com>
2022-06-05 13:33:49 +0400

    fix: dont add name to content comp

--- src/entities/schema/lib/create-new-comp.ts
+++ src/entities/schema/lib/create-new-comp.ts
@@ -1,13 +1,22 @@
+import { assertNotUndefined } from '@savchenko91/schema-validator'
+
+import { componentListBlind } from '../schema-drawer/lib/component-list'
 import uniqid from 'uniqid'
 
 import { Comp, Schema } from '@/entities/schema'
 
 export default function createNewComp(schema: Schema): Comp {
+  const component = componentListBlind[schema.componentName || '']
+  assertNotUndefined(component)
+
   const comp: Comp = {
     id: uniqid(),
-    name: uniqid(),
     title: schema.title,
     compSchemaId: schema.id,
+  }
+
+  if (component.type === 'content') {
+    comp.name = uniqid()
   }
 
   // const defaultValueComp = Object.values(schema.comps).find((comp) => comp.defaultValue !== undefined)
