30c98646a5c06bcbb7be6f736f7ca2fbf25e6eb8
Savchenko Alexander <sashulinator@gmail.com>
2022-06-11 10:02:00 +0400

    add focusHOC

---
+++ src/shared/focus-hoc/index.tsx
@@ -0,0 +1,51 @@
+import React, { LegacyRef, useEffect, useRef, useState } from 'react'
+
+interface FocusProps {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onFocus: (...args: any[]) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onBlur: (...args: any[]) => void
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ref: LegacyRef<Element | null>
+  isFocused: boolean
+}
+
+export function FocusHOC<P>(WrappedComponent: React.ComponentType<P & FocusProps>) {
+  const ComponentWithInterceptedFocus = (props: P & FocusProps) => {
+    const ref = useRef<null | Element>(null)
+
+    const [isFocused, setIsFocused] = useState(false)
+
+    useEffect(() => {
+      function handleFocus() {
+        props.onFocus()
+        setIsFocused(true)
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      function handleOutsideClick(event: any) {
+        if (!ref.current?.contains(event?.target)) {
+          props.onBlur()
+          setIsFocused(false)
+        }
+      }
+
+      document.addEventListener('click', handleOutsideClick)
+      ref.current?.addEventListener('click', handleFocus)
+
+      ref.current?.addEventListener('focusin', handleFocus)
+      document?.addEventListener('focusin', handleOutsideClick)
+
+      return () => {
+        document.removeEventListener('click', handleOutsideClick)
+        ref.current?.removeEventListener('click', handleFocus)
+
+        ref.current?.removeEventListener('focusin', handleFocus)
+        document?.removeEventListener('focusin', handleOutsideClick)
+      }
+    }, [])
+
+    return <WrappedComponent {...props} ref={ref} isFocused={isFocused} />
+  }
+  return ComponentWithInterceptedFocus
+}
