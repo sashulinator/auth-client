ce1544200f0fa30e985db4a8d1a57f5315f03b4a
Savchenko Alexander <sashulinator@gmail.com>
2022-06-18 16:08:27 +0400

    add remove

--- src/lib/schema/store.ts
+++ src/lib/schema/store.ts
@@ -2,23 +2,31 @@ import { Item, StoreAbstract, StoreData } from './store-abstract'
 import uniqid from 'uniqid'
 
 export class Store<TItem extends Item> extends StoreAbstract<TItem> {
-  constructor(entities: { [key: string | number]: TItem }, idKey: string | number = 'id') {
+  constructor(data: { [key: string | number]: TItem }, idKey: string | number = 'id') {
     super()
-    this.data = entities
+    this.data = data
     this.idKey = idKey
   }
 
-  add(entity: TItem) {
-    const newCatalog = { ...this.data, [this.idKeyValue(entity)]: entity }
+  forEach(cb: (item: TItem, key: string, entities: StoreData<TItem>) => void) {
+    for (let index = 0; index < this.entries.length; index++) {
+      const [key, item] = this.entries[index] as [string, TItem]
+
+      cb(item, key, this.data)
+    }
+  }
+
+  add(item: TItem) {
+    const newCatalog = { ...this.data, [this.idKeyValue(item)]: item }
 
     this.data = newCatalog
   }
 
   copy(id: string, uniqKeys: string[] = []): TItem {
     const newUniqKeys = ['id', ...uniqKeys]
-    const entity = this.data[id]
+    const item = this.data[id]
 
-    if (entity === undefined) {
+    if (item === undefined) {
       throw new Error('Entity does not exists')
     }
 
@@ -27,14 +35,12 @@ export class Store<TItem extends Item> extends StoreAbstract<TItem> {
         ...acc,
         [keyName]: uniqid(),
       }
-    }, entity)
+    }, item)
   }
 
-  forEach(cb: (entity: TItem, key: string, entities: StoreData<TItem>) => void) {
-    for (let index = 0; index < this.entries.length; index++) {
-      const [key, entity] = this.entries[index] as [string, TItem]
-
-      cb(entity, key, this.data)
-    }
+  remove(itemId: string) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { [itemId]: removedProperty, ...newData } = this.data
+    this._data = newData
   }
 }
