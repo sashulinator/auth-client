f2bfc39cc857676f1916e6207e5d5ac5f353ed9b
Savchenko Alexander <sashulinator@gmail.com>
2022-06-19 14:53:04 +0400

    add merge

---
+++ src/lib/merge.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line @typescript-eslint/ban-types
+type OptionalPropertyNames<T> = { [K in keyof T]-?: {} extends { [P in K]: T[K] } ? K : never }[keyof T]
+
+type SpreadProperties<L, R, K extends keyof L & keyof R> = { [P in K]: L[P] | Exclude<R[P], undefined> }
+
+type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
+
+type SpreadTwo<L, R> = Id<
+  Pick<L, Exclude<keyof L, keyof R>> &
+    Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
+    Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
+    SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
+>
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Spread<A extends readonly [...any]> = A extends [infer L, ...infer R] ? SpreadTwo<L, Spread<R>> : unknown
+
+export default function merge<A extends object[]>(...a: [...A]) {
+  return Object.assign({}, ...a) as Spread<A>
+}
