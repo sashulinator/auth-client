2f84ebbc4074c181f28d8f6bc1099b8c83b6b0fe
Savchenko Alexander <sashulinator@gmail.com>
2022-07-11 14:20:35 +0400

    header fix

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -19,50 +19,58 @@ export default function Header(): JSX.Element | null {
   }
 
   return (
-    <Stack as="header" horizontal verticalAlign="center" className="Header" tokens={{ padding: '16px 16px' }}>
-      <div
-        className="logo"
-        style={{ minWidth: 'calc(var(--navPanel_size) * 1px)', maxWidth: 'calc(var(--navPanel_size) * 1px)' }}
+    <header>
+      <Stack
+        horizontal
+        verticalAlign="center"
+        className="Header"
+        tokens={{ padding: '16px 16px' }}
+        style={{ width: '100%', height: '100%' }}
       >
-        <Icon iconName="DataTech" />
-      </div>
-      <Stack style={{ width: '100%', background: 'var(--white)' }}>
-        <Stack
-          horizontal
-          className={HEADER_PORTAL_LEFT_CLASSNAME.replace('.', '')}
-          horizontalAlign="start"
-          verticalAlign="center"
-          style={{ width: '100%' }}
-        />
-        <Stack
-          horizontal
-          className={HEADER_PORTAL_CENTER_CLASSNAME.replace('.', '')}
-          horizontalAlign="start"
-          verticalAlign="center"
-          style={{ width: '100%' }}
-        />
-        <Stack
-          horizontal
-          className={HEADER_PORTAL_RIGHT_CLASSNAME.replace('.', '')}
-          horizontalAlign="end"
-          verticalAlign="center"
-          style={{ width: 'fit-content' }}
-        />
+        <div
+          className="logo"
+          style={{ minWidth: 'calc(var(--navPanel_size) * 1px)', maxWidth: 'calc(var(--navPanel_size) * 1px)' }}
+        >
+          <Icon iconName="DataTech" />
+        </div>
+        <Stack style={{ width: '100%', background: 'var(--white)' }}>
+          <Stack
+            horizontal
+            className={HEADER_PORTAL_LEFT_CLASSNAME.replace('.', '')}
+            horizontalAlign="start"
+            verticalAlign="center"
+            style={{ width: '100%' }}
+          />
+          <Stack
+            horizontal
+            className={HEADER_PORTAL_CENTER_CLASSNAME.replace('.', '')}
+            horizontalAlign="start"
+            verticalAlign="center"
+            style={{ width: '100%' }}
+          />
+          <Stack
+            horizontal
+            className={HEADER_PORTAL_RIGHT_CLASSNAME.replace('.', '')}
+            horizontalAlign="end"
+            verticalAlign="center"
+            style={{ width: 'fit-content' }}
+          />
+        </Stack>
+        <Stack as="ul" horizontal verticalAlign="center" tokens={{ childrenGap: 16 }}>
+          <li>
+            <Link to={ROUTES.USER_PROFILE.PATH}>
+              <Persona
+                text="Юрий Кнорозов"
+                size={PersonaSize.size32}
+                imageUrl="https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/%D0%AE%D1%80%D0%B8%D0%B9_%D0%9A%D0%BD%D0%BE%D1%80%D0%BE%D0%B7%D0%BE%D0%B2.jpg/548px-%D0%AE%D1%80%D0%B8%D0%B9_%D0%9A%D0%BD%D0%BE%D1%80%D0%BE%D0%B7%D0%BE%D0%B2.jpg"
+              />
+            </Link>
+          </li>
+          <li>
+            <LogoutButton />
+          </li>
+        </Stack>
       </Stack>
-      <Stack as="ul" horizontal verticalAlign="center" tokens={{ childrenGap: 16 }}>
-        <li>
-          <Link to={ROUTES.USER_PROFILE.PATH}>
-            <Persona
-              text="Юрий Кнорозов"
-              size={PersonaSize.size32}
-              imageUrl="https://upload.wikimedia.org/wikipedia/ru/thumb/4/4c/%D0%AE%D1%80%D0%B8%D0%B9_%D0%9A%D0%BD%D0%BE%D1%80%D0%BE%D0%B7%D0%BE%D0%B2.jpg/548px-%D0%AE%D1%80%D0%B8%D0%B9_%D0%9A%D0%BD%D0%BE%D1%80%D0%BE%D0%B7%D0%BE%D0%B2.jpg"
-            />
-          </Link>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </Stack>
-    </Stack>
+    </header>
   )
 }
